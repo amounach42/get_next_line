99da28f4973a516244259cd63d9126e598197ca0
Abdessalam Mounach <amounach@e2r4p8.1337.ma>
2022-02-25 18:22:05 +0100

    chore: gnl utils ft_strjoin

--- get_next_line.utils.c
+++ get_next_line.utils.c
@@ -52,3 +52,29 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	str[i] = '\0';
 	return (str);
 }
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+	int		str_len;
+
+	if (!s1 || !s2)
+		return (NULL);
+	str_len = ft_strlen(s1) + ft_str_len(s2);
+   	i = 0;
+	j = 0;
+	str = (char *) malloc((str_len + 1) * sizeof(char));
+		if (!str) 
+			return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+		str[i++] = s2[j++];
+	str[i] = 0;
+	return (str);
+}
